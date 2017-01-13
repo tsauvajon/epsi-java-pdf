e9b0b7a0ce39df7633cb68f967169b369845034d
Thomas SAUVAJON <thomas.sauvajon@gmail.com>
2017-01-13 14:56:47 +0100

    Virage de lignes
    J'ai retiré des lignes inutiles

--- src/pdf/PDFWithText.java
+++ src/pdf/PDFWithText.java
@@ -59,37 +59,6 @@ public class PDFWithText {
 
     public static void main(String[] args) {
 //        createMockPdf();
-////Create a file chooser
-//        final JFileChooser fc = new JFileChooser();
-//
-////In response to a button click:
-//        int returnVal = fc.showOpenDialog(aComponent);
-
-//    for (int i = 0; i < args.length; i++) {
-//        switch (args[i].charAt(0)) {
-//        case '-':
-//            if (args[i].length() < 2)
-//                throw new IllegalArgumentException("Not a valid argument: "+args[i]);
-//            if (args[i].charAt(1) == '-') {
-//                if (args[i].length() < 3)
-//                    throw new IllegalArgumentException("Not a valid argument: "+args[i]);
-//                // --opt
-//                doubleOptsList.add(args[i].substring(2, args[i].length()));
-//            } else {
-//                if (args.length-1 == i)
-//                    throw new IllegalArgumentException("Expected arg after: "+args[i]);
-//                // -opt
-//                optsList.add(new Option(args[i], args[i+1]));
-//                i++;
-//            }
-//            break;
-//        default:
-//            // arg
-//            argsList.add(args[i]);
-//            break;
-//        }
-//    }
-
         PDDocument joined;
         PDDocument split;
         int begin = 2;
